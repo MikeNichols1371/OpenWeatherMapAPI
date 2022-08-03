3a580e397283b191f28d7cd041d69ebf3dc552c1
MikeNichols1371 <mnich13711@gmail.com>
2022-08-03 12:03:14 -0400

    Finished Exercise

---
+++ OpenWeatherMapAPI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();  
+
+Console.WriteLine("Enter your City Name");
+var cityName = Console.ReadLine();
+Console.WriteLine();
+
+Console.Write("Enter Api Key: ");
+var apiKey = Console.ReadLine();
+Console.WriteLine();
+
+var weatherApi = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
+
+
+var cityWeather = client.GetStringAsync(weatherApi).Result;
+
+var currentCityWeather = JObject.Parse(cityWeather).GetValue("main");
+
+Console.WriteLine($"The Weather of your City is :\n" +
+    $"Temp: {currentCityWeather["temp"]} \n" +
+    $"Feels Like: {currentCityWeather["feels_like"]} \n" +
+    $"Min Temp: {currentCityWeather["temp_min"]} \n" +
+    $"Max Temp: {currentCityWeather["temp_max"]} \n" +
+    $"Pressure: {currentCityWeather["pressure"]} \n" +
+    $"Humidity: {currentCityWeather["humidity"]}");
+    
+
+
+
